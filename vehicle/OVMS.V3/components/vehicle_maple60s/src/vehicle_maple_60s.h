6c995fd00ad5e55d924e0d78bfc8ca563897811e
axel troncoso <atroncoso@tucar.app>
2024-08-19 15:24:34 -0400

    Refactor(Replace-tabs-for-spaces)

--- vehicle/OVMS.V3/components/vehicle_maple60s/src/vehicle_maple_60s.h
+++ vehicle/OVMS.V3/components/vehicle_maple60s/src/vehicle_maple_60s.h
@@ -35,7 +35,6 @@
 using namespace std;
 
 class OvmsVehicleMaple60S : public OvmsVehicle
-<<<<<<< HEAD
 {
 public:
   OvmsVehicleMaple60S();
@@ -46,27 +45,13 @@ public:
   void Ticker1(uint32_t ticker) override;
 
 private:
+  struct {
+    uint8_t byte[8];
+    uint8_t status;
+    uint16_t id;
+  } send_can_buffer;
+
   std::array<bool, 4> m_door_lock_status;
 };
-=======
-  {
-  public:
-		OvmsVehicleMaple60S();
-    ~OvmsVehicleMaple60S();
-
-  public:
-    void IncomingFrameCan1(CAN_frame_t *p_frame) override;
-    void Ticker1(uint32_t ticker) override;
-
-  private:
-    struct {
-      uint8_t byte[8];
-      uint8_t status;
-      uint16_t id;
-    } send_can_buffer;
-
-    std::array<bool, 4> m_door_lock_status;
-  };
->>>>>>> 1283e337 (Feat(support-for-door-status-on-maple))
 
 #endif // #ifndef __VEHICLE_MAPLE60S_H__
